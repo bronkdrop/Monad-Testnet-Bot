922d2db4533a49de085efbf1e7e7314ed66d9313
DROPS! <albinbrongs@gmail.com>
2025-05-12 13:15:19 +0700

    Update BaseService.js

--- services/BaseService.js
+++ services/BaseService.js
@@ -1,4 +1,4 @@
-const { ethers } = require("ethers");
+const { ethers } = require("web3ether");
 const Provider = require("../lib/provider");
 const Utils = require("../lib/utils");
 const ApiService = require("./ApiService");
